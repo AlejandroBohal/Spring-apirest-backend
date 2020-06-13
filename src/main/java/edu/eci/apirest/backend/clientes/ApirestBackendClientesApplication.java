3b7d6a64033f0e65750d367bb1a570316fbe3565
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-06-13 17:15:56 -0500

    Initial commit

---
+++ src/main/java/edu/eci/apirest/backend/clientes/ApirestBackendClientesApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.apirest.backend.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBackendClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBackendClientesApplication.class, args);
+	}
+
+}
