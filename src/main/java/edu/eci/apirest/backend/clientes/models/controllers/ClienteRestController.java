7baa8c14099c288fe9b58e838e9664da9f8a67af
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-06-27 18:54:02 -0500

    gitflow-feature-stash: Sp02us003-ValidationServerSidePM
    Pruebas con postman, solucionadas dependencias

--- src/main/java/edu/eci/apirest/backend/clientes/models/controllers/ClienteRestController.java
+++ src/main/java/edu/eci/apirest/backend/clientes/models/controllers/ClienteRestController.java
@@ -1,5 +1,6 @@
 package edu.eci.apirest.backend.clientes.models.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,16 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import edu.eci.apirest.backend.clientes.models.entity.Cliente;
 import edu.eci.apirest.backend.clientes.models.services.IClienteService;
@@ -62,10 +54,11 @@ public class ClienteRestController {
 		Cliente clienteNuevo = null;
 		Map<String,Object> response = new HashMap<>();
 		if (result.hasErrors()){
+			System.out.println("Hola");
 			/*List<String> errors = new ArrayList<>();
 			for (FieldError err : result.getFieldErrors()){
 				errors.add("El campo '" + err.getField() + "' "+err.getDefaultMessage());
-			} jdk < 8*/
+			}* jdk <8*/
 			List<String> errors = result.getFieldErrors()
 				.stream()
 				.map(err -> "El campo '" + err.getField()+"' " +err.getDefaultMessage())
